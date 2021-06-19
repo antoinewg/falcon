92c7d9f5cbb6deca0671296984cea2c853471a95
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-19 12:43:26 +0200

    Add basic page design

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,51 @@
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import Container from '@material-ui/core/Container'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import Grid from '@material-ui/core/Grid'
+import { makeStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+import React from 'react'
+
 import { Graph } from './components/Graph'
 
-export const App = () => (
-  <div style={{ height: '100vh' }}>
-    <Graph />
-  </div>
-)
+export const App = () => {
+  const classes = useStyles()
+
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <main>
+        <Container className={classes.cardGrid} maxWidth="xl">
+          <Grid container spacing={4}>
+            <Grid item xs={12} sm={6} md={4}>
+              <Card className={classes.card}>
+                <CardContent className={classes.cardContent}>
+                  <Typography gutterBottom variant="h5" component="h2">
+                    Planetary system
+                  </Typography>
+                  <Graph />
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </main>
+    </React.Fragment>
+  )
+}
+
+const useStyles = makeStyles((theme) => ({
+  cardGrid: {
+    paddingTop: theme.spacing(8),
+    paddingBottom: theme.spacing(8),
+  },
+  card: {
+    height: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  cardContent: {
+    minHeight: 400,
+  },
+}))

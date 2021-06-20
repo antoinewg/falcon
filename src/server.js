f05918504d6e156d061dfda63a46ba5c6ea0724c
Antoine Garcia <antoine.garcia@polyconseil.fr>
2021-06-20 11:18:49 +0200

    Handle relative / absolute paths

--- src/server.js
+++ src/server.js
@@ -15,7 +15,13 @@ server.get('/routes/:id', (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000')
   if (!EXAMPLES.includes(req.params.id)) return res.status(400).json({ error })
 
-  const database = `${PATH + req.params.id}/universe.db`
+  const file = `${PATH + req.params.id}/millenium-falcon.json`
+
+  const data = JSON.parse(fs.readFileSync(file, 'utf8'))
+  const routes_db = data['routes_db']
+
+  // Check if path is absolute or relative
+  const database = routes_db.includes('/') ? routes_db : `${PATH + req.params.id}/${routes_db}`
   const db = new sqlite3.Database(database, sqlite3.OPEN_READWRITE)
   db.all('select * from ROUTES', [], (_, routes) => res.json({ routes }))
   db.close()
